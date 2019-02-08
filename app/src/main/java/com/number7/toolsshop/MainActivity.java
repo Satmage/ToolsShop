a5ca47aa2f4192b41c8263821eb45b4369e8162e
Andrei Ivanou <Kleckers@yandex.ru>
2019-02-08 18:30:44 +0300

    update MainActivity.java - hide ActionBar - add handler tabs
    update activity_main.xml
    - add ListViewTools
    - add ImageViewLogo
    
    update strings.xml

--- app/src/main/java/com/number7/toolsshop/MainActivity.java
+++ app/src/main/java/com/number7/toolsshop/MainActivity.java
@@ -1,13 +1,38 @@
 package com.number7.toolsshop;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewTolls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ссылка на текущий ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
+        listViewTolls = findViewById(R.id.listViewTools);
+        // Добавляем слушатель событий
+        listViewTolls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            // onItemClick - что сделать при нажатии на элемент списка
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
+
+
 }
